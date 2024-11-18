a814b519bd0002ca0b56897807e219c82f79b7e9
Murrrphy <444575716@qq.com>
2024-11-18 23:08:52 +0800

    match KeyEvent to quit when entered ctrl+q

--- src/editor.rs
+++ src/editor.rs
@@ -1,35 +1,43 @@
 use crossterm::{
-    event::{read, Event::Key, KeyCode::Char}, 
+    event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers}, 
     terminal::{disable_raw_mode, enable_raw_mode}
 };
 
-pub struct Editor{}
+pub struct Editor{
+    quit: bool,
+}
 
 impl Editor {
     /// default constructor
     pub fn default() -> Self {
-        Editor{}
+        Editor{ quit: false }
     }
     /// start functioning
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         if let Err(e) = self.repl() {
             panic!("{e:#?}");
         }
         print!("Thanks for using!");
     }
     /// read-eval-print-loop
-    fn repl(&self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         // throw errors to upper layers
         enable_raw_mode()?;
         loop {
             // read an event, including character event, mouse event, .etc
-            if let Key(event) = read()? {
-                println!("{event:?} \r");
-                if let Char(c) = event.code {
-                    if c == 'q' {
-                        break;
-                    }
+            if let Key(KeyEvent{ 
+                code, modifiers, kind, state
+            }) = read()? {
+                println!("code: {code:?}, modifiers: {modifiers:?}, kind: {kind:?}, state: {state:?} \r");
+                match code {
+                    Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.quit = true;
+                    },
+                    _ => (),
                 }
+            }
+            if self.quit == true {
+                break;
             }
         }
         disable_raw_mode()?;
