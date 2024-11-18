4dd6ba178293ff8aa97d4c05ed6c5858f63cc0fb
Murrrphy <444575716@qq.com>
2024-11-18 23:55:37 +0800

    split repl into mutiple methods; added initializing and terminating tasks

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,9 @@
 use crossterm::{
-    event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers}, 
-    terminal::{disable_raw_mode, enable_raw_mode}
+    event::{read, Event::{self, Key}, KeyCode::Char, KeyEvent, KeyModifiers}, 
+    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType}, 
+    execute,
 };
+use std::io::stdout;
 
 pub struct Editor{
     quit: bool,
@@ -11,36 +13,54 @@ impl Editor {
     /// default constructor
     pub fn default() -> Self {
         Editor{ quit: false }
-    }
+    } 
     /// start functioning
     pub fn run(&mut self) {
-        if let Err(e) = self.repl() {
-            panic!("{e:#?}");
-        }
-        print!("Thanks for using!");
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+    /// do some initializing job
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+    /// do some job before exiting
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
     }
     /// read-eval-print-loop
     fn repl(&mut self) -> Result<(), std::io::Error> {
-        // throw errors to upper layers
-        enable_raw_mode()?;
         loop {
-            // read an event, including character event, mouse event, .etc
-            if let Key(KeyEvent{ 
-                code, modifiers, kind, state
-            }) = read()? {
-                println!("code: {code:?}, modifiers: {modifiers:?}, kind: {kind:?}, state: {state:?} \r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.quit = true;
-                    },
-                    _ => (),
-                }
-            }
+            let event = read()?;
+            self.evaluate_event(&event);
+
             if self.quit == true {
+                Self::clear_screen()?;
+                print!("Thanks for using");
                 break;
             }
         }
-        disable_raw_mode()?;
         Ok(())
+    }
+    /// evaluate an event, including character event, mouse event, .etc
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent{ 
+            code, modifiers, ..
+        }) = event {
+            println!("[debug]: code: {code:?}, modifiers: {modifiers:?} \r");
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.quit = true;
+                },
+                _ => (),
+            }
+        }
+    }
+    /// clear the screen, send "/x1b[kJ" to the terminal, where k is 1, 2 or NaN
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
     }
 }
