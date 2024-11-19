26610459a7c2a4157223a8279900a61a90fdb660
Murrrphy <444575716@qq.com>
2024-11-19 20:56:32 +0800

    assignment4 ok, implement caret movement. implmented refresh_screen at last, because I found it would be very useful in the future. now, when refresh the screen, terminal set caret position as well.

--- src/editor.rs
+++ src/editor.rs
@@ -1,20 +1,29 @@
 mod terminal;
 
-use crossterm::event::{read, Event::{self, Key}, KeyCode::Char, KeyEvent, KeyModifiers};
-use std::io::Error as IoE;
-use terminal::Terminal;
+use crossterm::event::{read, Event::{self, Key}, KeyCode::{self, *}, KeyEvent, KeyModifiers};
+use std::{cmp::min, io::Error as IoE};
+use terminal::{Position, Size, Terminal};
 
 pub struct Editor{
+    // whether use entered ctrl+q
     quit: bool,
+    // current position of the cursor
+    position: Position,
 }
 
 impl Editor {
     /// default constructor
     pub fn default() -> Self {
-        Editor{ quit: false }
+        Editor{ 
+            quit: false,
+            position: Position::default(),
+        }
     } 
     /// start functioning
     pub fn run(&mut self) {
+        if Terminal::size().unwrap().height < 2 {
+            panic!("Too small terminal!");
+        }
         Terminal::initialize().unwrap();
         let result = self.repl();
         Terminal::terminate().unwrap();
@@ -24,7 +33,9 @@ impl Editor {
     fn repl(&mut self) -> Result<(), IoE> {
         loop {
             let event = read()?;
-            self.evaluate_event(&event);
+            // update after user enters something
+            self.evaluate_event(&event)?;
+            self.refresh_screen()?;
 
             if self.quit {
                 break;
@@ -32,18 +43,79 @@ impl Editor {
         }
         Ok(())
     }
-    /// evaluate an event, including character event, mouse event, .etc
-    fn evaluate_event(&mut self, event: &Event) {
+    /// evaluate an event and distribute to corresponding method
+    fn evaluate_event(&mut self, event: &Event) -> Result<(), IoE> {
         if let Key(KeyEvent{ 
-            code, modifiers, ..
+            code, 
+            modifiers, 
+            kind: _, 
+            state: _,
         }) = event {
-            println!("[debug]: code: {code:?}, modifiers: {modifiers:?} \r");
+            // println!("[debug]: code: {code:?}, modifiers: {modifiers:?} \r");
             match code {
                 Char('q') if *modifiers == KeyModifiers::CONTROL => {
                     self.quit = true;
                 },
+                Up | Down | Left | Right | PageUp | PageDown | Home | End => {
+                    self.move_cursor(*code)?;
+                },
                 _ => (),
             }
         }
+        Ok(())
+    }
+    fn move_cursor(&mut self, code: KeyCode) -> Result<(), IoE> {
+        let Size { width, height } = Terminal::size()?;
+        let Position { mut x, mut y } = self.position;
+        match code {
+            Up => {
+                y = y.saturating_sub(1);
+            },
+            Down => {
+                y = min(y + 1, height);
+            },
+            Left => {
+                x = x.saturating_sub(1);
+            },
+            Right => {
+                x = min(x + 1, width);
+            }
+            PageUp => {
+                y = 0;
+            },
+            PageDown => {
+                y = height;
+            },
+            Home => {
+                x = 0;
+            },
+            End => {
+                x = width;
+            }
+            _ => (),
+        }
+        self.position = Position{ x, y };
+        Ok(())
+    }
+    fn refresh_screen(&self) -> Result<(), IoE> {
+        Terminal::hide_cursor()?;
+        self.draw_rows()?;
+        Terminal::move_cursor(Position {
+            x: self.position.x,
+            y: self.position.y,
+        })?;
+        Terminal::show_cursor()?;
+        Terminal::execute()
+    }
+    /// draw '~' at the start of each line
+    pub fn draw_rows(&self) -> Result<(), IoE> {
+        Terminal::reset_cursor()?;
+        Terminal::clear_screen()?;
+        let y = Terminal::size().unwrap().height;
+        for _ in 0..y {
+            Terminal::print("~\r\n")?;
+        }
+        // Self::reset_cursor()?;
+        Terminal::execute()
     }
 }
--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -3,10 +3,19 @@ use crossterm::{queue, style::Print, Command};
 use crossterm::cursor::{Hide, MoveTo, Show};
 use crossterm::terminal::{self, disable_raw_mode, enable_raw_mode, Clear, ClearType};
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
+
 #[derive(Clone, Copy)]
 pub struct Position {
     pub x: u16,
     pub y: u16,
+}
+
+impl Position {
+    pub fn default() -> Position {
+        Position{ x: 0, y: 0 }
+    }
 }
 
 #[allow(unused)]
@@ -19,15 +28,14 @@ pub struct Size {
 pub struct Terminal();
 
 impl Terminal {
-    /// do some initializing job
+    /// do some initializing work
     pub fn initialize() -> Result<(), IoE> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::draw_rows()?;
         Self::welcome_message()?;
         Self::execute()
     }
-    /// do some job before exiting
+    /// do some work before exiting
     pub fn terminate() -> Result<(), IoE> {
         Self::execute()?;
         Self::clear_screen()?;
@@ -35,38 +43,21 @@ impl Terminal {
         Self::print("\x1b[32mThanks for using! \r\n")?;
         disable_raw_mode()
     }
+    // draw welcome message; part of initializing work
     fn welcome_message() -> Result<(), IoE> {
-        let str1 = "Welcome!";
-        let str2 = "My-hecto    version: 0.1.0";
+        let msg = format!("{NAME} -- version {VERSION}");
         let size = Self::size()?;
-        let len1 = str1.len() as u16;
-        let len2 = str2.len() as u16;
+        let len = msg.len() as u16;
         let start_row = size.height / 3 * 2;
-        let start_col1 = (size.width - len1) / 2;
-        let start_col2 = (size.width - len2) / 2;
-        Self::move_cursor(Position {x: start_col1, y: start_row })?;
-        Self::print(str1)?;
-        Self::move_cursor(Position { x: start_col2, y: start_row + 1 })?;
-        Self::print(str2)?;
+        let start_col = (size.width - len) / 2;
+        Self::move_cursor(Position {x: start_col, y: start_row })?;
+        Self::print(&msg)?;
         Self::reset_cursor()?;
         Self::execute()
     }
     /// execute all command from command queue
-    fn execute() -> Result<(), IoE> {
+    pub fn execute() -> Result<(), IoE> {
         stdout().flush()?;
-        Ok(())
-    }
-    /// draw '~' at the start of each line
-    fn draw_rows() -> Result<(), IoE> {
-        Self::hide_cursor()?;
-        Self::reset_cursor()?;
-
-        let y = Self::size().unwrap().height;
-        for _ in 0..y {
-            Self::print("~\r\n")?;
-        }
-        Self::reset_cursor()?;
-        Self::show_cursor()?;
         Ok(())
     }
     /// clear the screen, send "/x1b[kJ" to the terminal, where k is 1, 2 or NaN
@@ -76,30 +67,31 @@ impl Terminal {
         Self::show_cursor()
     }
     /// size of current terminal
-    fn size() -> Result<Size, IoE> {
+    pub fn size() -> Result<Size, IoE> {
         let (width, height) = terminal::size()?;
         Ok(Size { width, height })
     }
     /// reset cursor to the top left
-    fn reset_cursor() -> Result<(), IoE> {
+    pub fn reset_cursor() -> Result<(), IoE> {
         Self::move_cursor(Position { x: 0, y: 0 })
     }
     /// move cursor to given position
-    fn move_cursor(position: Position) -> Result<(), IoE> {
+    pub fn move_cursor(position: Position) -> Result<(), IoE> {
         Self::push_command_queue(MoveTo(position.x, position.y))
     }
     /// hide terminal cursor. operation will be written in terminal buffer
-    fn hide_cursor() -> Result<(), IoE> {
+    pub fn hide_cursor() -> Result<(), IoE> {
         Self::push_command_queue(Hide)
     }
     /// show terminal cursor. operation will be written in terminal buffer
-    fn show_cursor() -> Result<(), IoE> {
+    pub fn show_cursor() -> Result<(), IoE> {
         Self::push_command_queue(Show)
     }
     /// use 'Print' with queue! instead of execute!
-    fn print(string: &str) -> Result<(), IoE> {
+    pub fn print(string: &str) -> Result<(), IoE> {
         Self::push_command_queue(Print(string))
     }
+    /// push a command to the command queue
     fn push_command_queue<T: Command>(command: T) -> Result<(), IoE> {
         queue!(stdout(), command)?;
         Ok(())
