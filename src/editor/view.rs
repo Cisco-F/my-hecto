0a68ff8b092fe8169143f6cbe954756f0c81eb32
Murrrphy <444575716@qq.com>
2024-11-20 11:30:39 +0800

    fix: terminal's row range is from 0 to height. so if current row is height-1, print a "\r\n" will result in one line's down scroll, which would make "Hellow world" invisible.

--- src/editor/view.rs
+++ src/editor/view.rs
@@ -10,16 +10,21 @@ impl View {
     /// render the terminal window
     pub fn render() -> Result<(), IoE> {
         Terminal::clear_line()?;
+        // Terminal::move_cursor(Position { x: 0, y: 1 })?;
         Terminal::print("Hello world!\r\n")?;
         let Size { height, .. } = Terminal::size()?;
-        for y in 2..height {
+        for y in 1..height {
             Terminal::clear_line()?;
             if y == height / 3 * 2 {
                 Self::draw_welcome_message()?;
             } else {
                 Self::draw_empty_row()?;
             }
+            if y.saturating_add(1) < height {
+                Terminal::print("\r\n")?;
+            }
         }
+        
         Terminal::execute()
     }
     /// draw welcome message; part of initializing work
@@ -28,13 +33,13 @@ impl View {
         let width = Terminal::size()?.width;
         let padding = (width - msg.len() as u16) / 2;
         let spaces = " ".repeat(padding as usize);
-        let msg = format!("~{spaces}{msg}\r\n");
+        let msg = format!("~{spaces}{msg}");
         Terminal::print(&msg)?;
         Terminal::execute()
     }
     /// draw a '~' at the start of the line
     fn draw_empty_row() -> Result<(), IoE> {
-        Terminal::print("~\r\n")?;
+        Terminal::print("~")?;
         Ok(())
     }
 }
